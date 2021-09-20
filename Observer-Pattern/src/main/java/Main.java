f1c0e808074c510c0dd1f52a80fb62a0ad11eb43
shiguri <yeguangpeng2018@outlook.com>
2021-09-20 08:27:23 +0800

    修改观察者模式，main函数

--- Observer-Pattern/src/main/java/Main.java
+++ Observer-Pattern/src/main/java/Main.java
@@ -8,6 +8,7 @@ import subject.Weather;
 import java.util.Random;
 
 public class Main {
+    /*
     public static void main(String[] args) {
         Subject weatherSubject = new Weather();
         Observer currentInfo = new CurrentConditionDisplay(weatherSubject);
@@ -30,6 +31,26 @@ public class Main {
             System.out.printf("第%s次 : ", i + 1);
             currentDisplay.display();
             changeDisplay.display();
+        }
+    }
+
+     */
+    public static void main(String[] args) {
+        Weather weather = new Weather();
+        CurrentConditionDisplay currentInfo = new CurrentConditionDisplay(weather);
+        WeatherChangeDisplay changeInfo = new WeatherChangeDisplay(weather);
+
+        int times = 5;
+        for (int i = 0; i < times; i++) {
+            Random random = new Random();
+            double temperature = random.nextDouble();
+            double humidity = random.nextDouble();
+            double pressure = random.nextDouble();
+
+            weather.setMessage(temperature, humidity, pressure);
+            System.out.printf("第%s次 : ", i + 1);
+            currentInfo.display();
+            changeInfo.display();
         }
     }
 }
